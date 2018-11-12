66b1e53e74a6cd95c9b103362d8606572c63244b
AGiantSquid <ashley@greenkeytech.com>
2018-11-12 15:47:28 -0600

    add code to solve the captcha, test passes

--- 2017/day_one/day_one.py
+++ 2017/day_one/day_one.py
@@ -1,2 +1,10 @@
 def captcha_sequence_solver(int_sequence):
-    return 3
+  listified = list(str(int_sequence))
+  listified.append(listified[0])
+  prior_el = ''
+  sum_of_sequences = 0
+  for el in listified:
+    if el == prior_el:
+      sum_of_sequences += int(el)
+    prior_el = el
+  return sum_of_sequences

06839e5ef659c49ae0ef8627d159efdb01086a89
AGiantSquid <ashley@greenkeytech.com>
2018-12-02 23:22:14 -0600

    solve first part of challenge with tests

---
+++ 2018/day_one/day_one_a.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+URL='https://adventofcode.com/2018/day/1/input'
+
+# get my user-specific cookies
+for cookie_file in ['cookies.txt', '../cookies.txt', '../../cookies.txt']:
+  if os.path.isfile(cookie_file):
+    with open(cookie_file, 'r') as f:
+      COOKIES=f.read()
+
+
+def get_dataset_from_url(url, cookies, requests=requests):
+    """Get request to url with cookies for user data."""
+    resp = requests.get(
+        url,
+        cookies=dict(cookies_are=cookies)
+    )
+    # Filter empty results
+    return filter(None, resp.text.split('\n'))
+
+
+def get_end_frequency(frequency_changes):
+    """Sum numbers together to get result frequency."""
+    return sum(int(num) for num in frequency_changes)
+
+
+if __name__ == '__main__':
+    FREQUENCY_LIST = get_dataset_from_url(URL, COOKIES)
+    END_FREQUENCY = get_end_frequency(FREQUENCY_LIST)
+    print(END_FREQUENCY)  # 454

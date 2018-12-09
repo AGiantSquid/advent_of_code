dd62e13e52db18c6a0d552f856da9586c5278e23
AGiantSquid <ashley@greenkeytech.com>
2018-12-09 00:17:12 -0600

    add day two part b and tests

--- 2018/day_two/test_day_two.py
+++ 2018/day_two/test_day_two.py
@@ -1,4 +1,5 @@
 from day_two_a import checksum_generator
+from day_two_b import common_letters_in_correct_labels
 from day_two_test_data import EXPECTED_PAYLOAD
 from aoc_utils import get_dataset_from_url
 
@@ -35,3 +36,24 @@ def test_checksum_generator_real_data():
   label_data = LABEL_DATA_PAYLOAD
   result = checksum_generator(label_data)
   assert result == 4712
+
+
+def test_common_letters_in_correct_labels():
+  label_data = [
+    "abcde",
+    "fghij",
+    "klmno",
+    "pqrst",
+    "fguij",
+    "axcye",
+    "wvxyz",
+  ]
+  expected_result = 'fgij'
+  results = common_letters_in_correct_labels(label_data)
+  assert results[0] == expected_result
+
+
+def test_common_letters_in_correct_labels_real_data():
+  label_data = LABEL_DATA_PAYLOAD
+  results = common_letters_in_correct_labels(label_data)
+  assert results[0] == 'lufjygedpvfbhftxiwnaorzmq'

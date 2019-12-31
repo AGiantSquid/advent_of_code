f160be379e7241f65d4b86ba9ace8f75c5ecd339
AGiantSquid <ashley.shultz@gmail.com>
2019-12-31 17:47:18 -0600

    day two

--- 2019/day_3/day_3.go
+++ 2019/day_3/day_3.go
@@ -11,8 +11,12 @@ import (
 func main () {
 	filePath := "test.txt"
 	testString := readData(filePath)
+
 	partOne := DistanceOfNearestIntersection(testString)
 	fmt.Println(partOne)
+
+	partTwo := DistanceOfFirstIntersection(testString)
+	fmt.Println(partTwo)
 }
 
 type Coords struct {
@@ -24,41 +28,68 @@ func (c *Coords) mahattanDistance() int {
 	return Abs(c.X) + Abs(c.Y)
 }
 
+type IndexedCoords map [int]Coords
+
+type OverlappingCoordsWithIndexes struct {
+	Distance int
+	OverlappingCoords Coords
+}
+
 
 func DistanceOfNearestIntersection (instructionsString string) int {
-	// first line is wire one instructions, second line is wire two instructions
-	instructionsSlice := strings.Split(instructionsString, "\n")[:2]
+	overlaps := getOverlappingCoordsForTwoWires(instructionsString)
 
-	wireOneCoords := getTraversedCoords(instructionsSlice[0])
-	wireTwoCoords := getTraversedCoords(instructionsSlice[1])
-
-	overlaps := getOverlappingCoords(wireOneCoords, wireTwoCoords)
-
-	closestOverlap := overlaps[0]
+	closestOverlapManhattanDistance := overlaps[0].OverlappingCoords.mahattanDistance()
 
 	for _, overlap := range overlaps {
-		if closestOverlap.mahattanDistance() > overlap.mahattanDistance() {
-			closestOverlap = overlap
+		currentCoordsManhattanDistance := overlap.OverlappingCoords.mahattanDistance()
+		if currentCoordsManhattanDistance < closestOverlapManhattanDistance  {
+			closestOverlapManhattanDistance = currentCoordsManhattanDistance
 		}
 	}
 
-	result := closestOverlap.mahattanDistance()
-
-	return result
+	return closestOverlapManhattanDistance
 }
 
-// Convert string of comma separated instructions into list of coordinates
-// instructions comes in the form of a letter, then a number, eg. "R8,U5,L5,D3"
-func getTraversedCoords(instructionString string) []Coords {
-	s := strings.Split(instructionString, ",")
-	// cursor holds the last known position of our wire
-	cursorCoords := Coords{0,0}
+func DistanceOfFirstIntersection(instructionsString string) int {
+	overlaps := getOverlappingCoordsForTwoWires(instructionsString)
 
-	var resultCoords []Coords
+	minManhattanDistance := overlaps[0].Distance
+
+	for _, el := range overlaps {
+		if el.Distance < minManhattanDistance {
+			minManhattanDistance = el.Distance
+		}
+	}
+
+	return minManhattanDistance
+}
+
+func getOverlappingCoordsForTwoWires(instructionsString string) []OverlappingCoordsWithIndexes {
+	// first line is wire one instructions, second line is wire two instructions
+	instructionsSlice := strings.Split(instructionsString, "\n")[:2]
+
+	wireOneCoords := getTraversedCoordsWithIndex(instructionsSlice[0])
+	wireTwoCoords := getTraversedCoordsWithIndex(instructionsSlice[1])
+
+	overlaps := getOverlappingCoordsWithDistance(wireOneCoords, wireTwoCoords)
+
+	return overlaps
+}
+
+// Convert string of comma separated instructions into list of coordinates with distance travelled
+// instructions comes in the form of a letter, then a number, eg. "R8,U5,L5,D3"
+func getTraversedCoordsWithIndex(instructionString string) IndexedCoords {
+	s := strings.Split(instructionString, ",")
+
+	cursorCoords := Coords{0,0}
+	resultCoords := IndexedCoords{}
+	runningTotal := 0
 
 	for _, instruction := range s {
 		direction := instruction[:1]
 		amount, _ := strconv.Atoi(instruction[1:])
+
 		for i := 0; i < amount; i++ {
 			if direction == "R" {
 				cursorCoords.X++
@@ -72,29 +103,32 @@ func getTraversedCoords(instructionString string) []Coords {
 			if direction == "D" {
 				cursorCoords.Y--
 			}
-			resultCoords = append(resultCoords, cursorCoords)
+			runningTotal++
+			resultCoords[runningTotal] = cursorCoords
 		}
 	}
 
 	return resultCoords
-
 }
 
 // return all the coordinates that are overlapping
-func getOverlappingCoords(one, two []Coords) []Coords {
-	var overlappingCoords []Coords
+func getOverlappingCoordsWithDistance(one, two IndexedCoords) []OverlappingCoordsWithIndexes {
+	var overlappingCoords []OverlappingCoordsWithIndexes
 
-	for _, coords1 := range one {
-		for _, coords2 := range two {
+	for i, coords1 := range one {
+		for j, coords2 := range two {
 			if coords1 == coords2 {
-				overlappingCoords = append(overlappingCoords, coords1)
+				intersection := OverlappingCoordsWithIndexes{
+					i + j,
+					coords1,
+				}
+				overlappingCoords = append(overlappingCoords, intersection)
 			}
 		}
 	}
 
 	return overlappingCoords
 }
-
 
 
 // Abs returns the absolute value of x.

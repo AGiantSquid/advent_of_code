f3609219035a8bcd9702c3c02096846d91ce3a5d
AGiantSquid <ashley.e.shultz@gmail.com>
2020-12-02 19:19:18 -0800

    solve part 2 day 3

--- python/2018/day_3/day_3_test.py
+++ python/2018/day_3/day_3_test.py
@@ -1,5 +1,5 @@
 from aoc_utils import get_aoc_data_for_challenge
-from day_3 import get_overlapping_fabric
+from day_3 import get_overlapping_fabric, get_non_overlapping_pattern_id
 
 
 PUZZLE_INPUT = get_aoc_data_for_challenge(__file__)
@@ -21,3 +21,21 @@ def test_get_overlapping_fabric_2():
     res = get_overlapping_fabric(PUZZLE_INPUT)
 
     assert res == 112418
+
+
+def test_get_non_overlapping_pattern_id():
+    data = [
+        '#1 @ 1,3: 4x4',
+        '#2 @ 3,1: 4x4',
+        '#3 @ 5,5: 2x2',
+    ]
+
+    res = get_non_overlapping_pattern_id(data)
+
+    assert res == '3'
+
+
+def test_get_non_overlapping_pattern_id_2():
+    res = get_non_overlapping_pattern_id(PUZZLE_INPUT)
+
+    assert res == '560'

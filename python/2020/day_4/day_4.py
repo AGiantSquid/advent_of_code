f811cdec2e7c1bd5011b30fe22e378c10d5ebcd2
AGiantSquid <ashley.e.shultz@gmail.com>
2020-12-07 14:57:45 -0800

    solve part 2

--- python/2020/day_4/day_4.py
+++ python/2020/day_4/day_4.py
@@ -2,21 +2,48 @@
 '''
 Solves Advent of Code problem for day 4.
 '''
+import re
 from functools import reduce
 from typing import List
 
 from aoc_utils import get_aoc_data_for_challenge
 
 
+def range_validator(low, high):
+    def wrapped(x):
+        return low <= int(x) <= high
+    return wrapped
+
+
+def valid_height(x):
+    if len(x) <= 2:
+        return False
+    if x.endswith('cm'):
+        return range_validator(150, 193)(x[:-2])
+    return range_validator(59, 76)(x[:-2])
+
+
+def valid_hair(x):
+    return len(x) == 7 and re.match(r'#[a-f0-9]{6}', x) is not None
+
+
+def valid_eye(x):
+    return x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
+
+def valid_pid(x):
+    return len(x) == 9 and x.isdigit()
+
+
 PASSPORT_FIELDS = {
-    'byr': 'Birth Year',
-    'iyr': 'Issue Year',
-    'eyr': 'Expiration Year',
-    'hgt': 'Height',
-    'hcl': 'Hair Color',
-    'ecl': 'Eye Color',
-    'pid': 'Passport ID',
-    'cid': 'Country ID',
+    'byr': range_validator(1920, 2002),
+    'iyr': range_validator(2010, 2020),
+    'eyr': range_validator(2020, 2030),
+    'hgt': valid_height,
+    'hcl': valid_hair,
+    'ecl': valid_eye,
+    'pid': valid_pid,
+    'cid': lambda x: True,
 }
 
 REQUIRED_KEYS = [_ for _ in PASSPORT_FIELDS.keys() if _ != 'cid']
@@ -37,9 +64,21 @@ def valid_passport(passport: dict) -> bool:
     return not any([_ not in keys for _ in REQUIRED_KEYS])
 
 
-def valid_passport_count(data: List[str]) -> int:
+def valid_passport_2(passport: dict) -> bool:
+    keys = passport.keys()
+    missing_keys = any([_ not in keys for _ in REQUIRED_KEYS])
+
+    if missing_keys:
+        return False
+
+    return all([
+        PASSPORT_FIELDS[k](v)
+        for k, v in passport.items()
+    ])
+
+
+def valid_passport_count(data: List[str], validator: callable) -> int:
     def reducer(accum, el):
-        # print(accum, el)
         if not accum:
             d = to_dict(el)
             return (d,)
@@ -61,11 +100,16 @@ def valid_passport_count(data: List[str]) -> int:
 
     passport_data = reduce(reducer, data, tuple())
 
-    return len([_ for _ in passport_data if valid_passport(_)])
+    return len([_ for _ in passport_data if validator(_)])
+
+
 
 
 if __name__ == '__main__':
     puzzle_data = get_aoc_data_for_challenge(__file__, filter_nulls=False)
 
-    result = valid_passport_count(puzzle_data)
+    result = valid_passport_count(puzzle_data, valid_passport)
     print(result)  # 219
+
+    result = valid_passport_count(puzzle_data, valid_passport_2)
+    print(result)  # 127
--- python/2020/day_4/day_4_test.py
+++ python/2020/day_4/day_4_test.py
@@ -1,5 +1,5 @@
 from aoc_utils import get_aoc_data_for_challenge
-from day_4 import valid_passport_count
+from day_4 import valid_passport_count, range_validator, valid_height, valid_hair, valid_eye, valid_pid, valid_passport, valid_passport_2
 
 
 PUZZLE_DATA = get_aoc_data_for_challenge(__file__, filter_nulls=False)
@@ -20,10 +20,44 @@ data = [
     'iyr:2011 ecl:brn hgt:59in',
 ]
 
+
 def test_valid_passport_count():
-    res = valid_passport_count(data)
+    res = valid_passport_count(data, valid_passport)
     assert res == 2
 
+
 def test_valid_passport_count_puzzle_data():
-    res = valid_passport_count(PUZZLE_DATA)
+    res = valid_passport_count(PUZZLE_DATA, valid_passport)
     assert res == 219
+
+
+def test_validators():
+    rv = range_validator(1920, 2002)
+    assert rv('2002') is True
+    assert rv('2003') is False
+
+    assert valid_height('150cm') is True
+    assert valid_height('149cm') is False
+    assert valid_height('76in') is True
+
+    assert valid_hair('#fa0088') is True
+    assert valid_hair('#ha0088') is False
+    assert valid_hair('#aa0088a') is False
+
+    assert valid_eye('amb') is True
+    assert valid_eye('aoeu') is False
+
+    assert valid_pid('000123456') is True
+    assert valid_pid('00012345a') is False
+    assert valid_pid('a00123456') is False
+    assert valid_pid('a001234560') is False
+
+
+def test_valid_passport_count_2():
+    res = valid_passport_count(data, valid_passport_2)
+    assert res == 2
+
+
+def test_valid_passport_count_2_puzzle_data():
+    res = valid_passport_count(PUZZLE_DATA, valid_passport_2)
+    assert res == 127

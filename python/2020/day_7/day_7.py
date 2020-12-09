ecac3412bdd0ca206546e385eb35bf869333f4f7
AGiantSquid <ashley.e.shultz@gmail.com>
2020-12-08 18:22:30 -0800

    solve part 2

--- python/2020/day_7/day_7.py
+++ python/2020/day_7/day_7.py
@@ -3,7 +3,7 @@
 Solves Advent of Code problem for day 7.
 '''
 
-from typing import List
+from typing import List, Tuple
 
 from aoc_utils import get_aoc_data_for_challenge
 
@@ -11,7 +11,7 @@ from aoc_utils import get_aoc_data_for_challenge
 BAG_COLOR = 'shiny gold'
 
 
-def count_parent_possibilities(data: List[str]):
+def count_parent_possibilities(data: List[str]) -> int:
     bd = get_bag_dict(data)
 
     return len(get_parents(bd, BAG_COLOR, set()))
@@ -51,8 +51,49 @@ def clean_label(label: str) -> str:
     return no_num
 
 
+def count_children(data: List[str]) -> int:
+    bd = get_bag_dict_2(data)
+    return get_children(bd, BAG_COLOR, 0)
+
+
+def get_children(bag_dict, start, children):
+    v = bag_dict.get(start, [])
+
+    if v:
+        for _ in v:
+            if _:
+                for i in range(_[1]):
+                    children += 1
+                    children = get_children(bag_dict, _[0], children)
+
+    return children
+
+
+def get_bag_dict_2(data: List[str]) -> dict:
+    d = {}
+
+    for rule in data:
+        key = clean_label(rule.split('contain')[0])
+        rest = [clean_label_2(_) for _ in rule.split('contain')[1].split(', ')]
+        d[key] = rest
+
+    return d
+
+
+def clean_label_2(label: str) -> Tuple[str, int]:
+    no_bag = label.split('bag')[0].strip()
+    if no_bag.startswith('no other'):
+        return None
+
+    num, no_num = no_bag.split(' ', 1)
+    return no_num, int(num)
+
+
 if __name__ == '__main__':
     puzzle_data = get_aoc_data_for_challenge(__file__, filter_nulls=False)
 
     result = count_parent_possibilities(puzzle_data)
+    print(result)  # 274
+
+    result = count_children(puzzle_data)
     print(result)  # 274

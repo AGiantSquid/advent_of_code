c8ef1646e4965f0daa70a2efedfef8c5589e7339
AGiantSquid <ashley.e.shultz@gmail.com>
2020-12-09 13:11:05 -0800

    use chunk func in day 6

--- python/2020/day_6/day_6.py
+++ python/2020/day_6/day_6.py
@@ -3,44 +3,35 @@
 Solves Advent of Code problem for day 6.
 '''
 from functools import reduce
-from typing import Tuple, List
-from aoc_utils import get_aoc_data_for_challenge
+from typing import Tuple, List, Iterable
+from aoc_utils import get_aoc_data_for_challenge, chunk_aoc_data
 
 
 def sum_of_responses(data: List[str], unique=True) -> int:
     '''Return sum of responses from each cluster.'''
+    chunked = chunk_aoc_data(data)
+
     if unique:
-        sets_of_answers = get_sets_of_answers(data)
+        sets_of_answers = get_unique_letters_in_chunks(chunked)
     else:
-        sets_of_answers = get_sets_of_unanimous_answers(data)
+        sets_of_answers = get_common_letters_in_chunks(chunked)
 
     return sum([len(_) for _ in sets_of_answers])
 
 
-def get_sets_of_answers(data: List[str]) -> Tuple[set, ...]:
-    def reducer(accum, el):
-        if not el:
-            return accum + (set(),)
-
-        return accum[:-1] + (accum[-1] | set(el),)
-
-    return reduce(reducer, data, (set(),))
+def get_unique_letters_in_chunks(data: Tuple[Tuple[str]]) -> Iterable:
+    '''Get set of unique individual letters in chunks.'''
+    return (set(''.join(_)) for _ in data)
 
 
-def get_sets_of_unanimous_answers(data: List[str]) -> Tuple[set, ...]:
-    def reducer(accum, el):
-        if not el:
-            return accum + (None,)
+def get_common_letters_in_chunks(data: Tuple[Tuple[str]]) -> Iterable:
+    '''Get set of common letters in chunks.'''
+    sets = (
+        (set(_) for _ in el)
+        for el in data
+    )
 
-        last_el = accum[-1]
-
-        if last_el is None:
-            return accum[:-1] + (set(el),)
-
-        return accum[:-1] + (last_el & set(el),)
-
-    return reduce(reducer, data, (None,))
-
+    return (reduce(lambda x, y: x & y, s) for s in sets)
 
 
 if __name__ == '__main__':

e110e7a16b7d97e63b9f858fdfb07e5170cfe6c3
AGiantSquid <ashley.e.shultz@gmail.com>
2020-12-17 14:25:02 -0800

    solve part 2

--- python/2020/day_13/day_13.py
+++ python/2020/day_13/day_13.py
@@ -21,8 +21,61 @@ def part_1(data: List[str]):
     return bus_id * times[bus_id]
 
 
+def part_2_naive(data: List[str]):
+    busses = {
+        int(el): i
+        for i, el in enumerate(data[1].split(','))
+        if el != 'x'
+    }
+    incrementor = max(busses.keys())
+
+    inc_diff = busses[incrementor]
+
+    busses = {
+        k: v - inc_diff
+        for k, v in busses.items()
+    }
+
+    for k, v in busses.items():
+        if v > 0:
+            busses[k] = v - k
+    pos = incrementor
+    valid = False
+    while not valid:
+        for k, v in busses.items():
+            needed_offset = busses[k]
+
+            observed_offset = (pos % k) * -1
+
+            if needed_offset == observed_offset:
+                valid = True
+                continue
+
+            pos += incrementor
+            valid = False
+            break
+
+        if valid is True:
+            return pos + busses[list(busses.keys())[0]]
+
+    return busses
+
+
 def part_2(data: List[str]):
-    pass
+    busses = {
+        int(el): -i % int(el)
+        for i, el in enumerate(data[1].split(','))
+        if el != 'x'
+    }
+
+    iterator = 0
+    increment = 1
+    for bus, adj_offset in busses.items():
+        while iterator % bus != adj_offset:
+            iterator += increment
+        increment *= bus
+
+    return iterator
 
 
 if __name__ == '__main__':
@@ -31,5 +84,5 @@ if __name__ == '__main__':
     result = part_1(puzzle_data)
     print(result)  # 2298
 
-#    result = part_2(puzzle_data)
-#    print(result)
+    result = part_2(puzzle_data)
+    print(result)  # 783685719679632
--- python/2020/day_13/day_13_test.py
+++ python/2020/day_13/day_13_test.py
@@ -17,6 +17,30 @@ def test_part_1():
     assert res == 295
 
 
-def test_part_2_pd():
+def test_part_1_pd():
+    res = part_1(PUZZLE_DATA)
+    assert res == 2298
+
+
+def test_part_2():
+    res = part_2(DATA)
+    assert res == 1068781
+
+
+def test_part_2a():
+    data = [
+        ['17,x,13,19', 3417],
+        ['67,7,59,61', 754018],
+        ['67,x,7,59,61', 779210],
+        ['67,7,x,59,61', 1261476],
+        ['1789,37,47,1889', 1202161486],
+    ]
+
+    for d in data:
+        res = part_2(['', d[0]])
+        assert res == d[1]
+
+
+def test_part_2():
     res = part_2(PUZZLE_DATA)
-    assert res == 0
+    assert res == 783685719679632

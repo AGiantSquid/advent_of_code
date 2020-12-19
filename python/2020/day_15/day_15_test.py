02844c9d61079f584eff1ed0c152912d999ff4a9
AGiantSquid <ashley.e.shultz@gmail.com>
2020-12-18 17:19:37 -0800

    mark slow tests for ci

--- python/2020/day_15/day_15_test.py
+++ python/2020/day_15/day_15_test.py
@@ -1,6 +1,8 @@
 '''
 Demonstrates that code works for day 15.
 '''
+import pytest
+
 from aoc_utils import get_aoc_data_for_challenge
 from day_15 import part_1, part_2
 
@@ -25,6 +27,7 @@ def test_part_1_pd():
     assert res == 492
 
 
+@pytest.mark.slow
 def test_part_2():
     data = ['0,3,6']
 
@@ -32,6 +35,7 @@ def test_part_2():
     assert res == 175594
 
 
+@pytest.mark.slow
 def test_part_2_pd():
     res = part_2(PUZZLE_DATA)
     assert res == 63644
